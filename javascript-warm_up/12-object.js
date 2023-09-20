d67bc97863030089410da3d7007597520c719d36
Vegas90 <amosmwoni@gmail.com>
2023-09-20 09:30:09 +0300

    M

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+
+  value= 89;
--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing']
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 for (let i=0; i<myArray.length; i++){
 console.log(myArray[i]);
 }
