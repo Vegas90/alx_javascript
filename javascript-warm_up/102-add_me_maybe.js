7dead8c3a99726b93db7210cde6ad6febbe328de
Vegas90 <amosmwoni@gmail.com>
2023-09-21 21:46:15 +0300

    M

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function addMeMaybe(number, theFunction)
 {
-    const number = number + 1;
-    theFunction(number)
+    const number1 = number + 1;
+    theFunction(number1)
 };
 
 module.exports = {addMeMaybe};
