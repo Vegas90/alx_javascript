36702e906a89388efcfdfd95b199b3510d544eb4
Vegas90 <amosmwoni@gmail.com>
2023-09-21 22:09:52 +0300

    M

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -5,12 +5,15 @@ const myObject = {
     value: 12,
 incr: function()
   {
+    //this keyword to access the value property within the incr method. 
 this.value = this.value +1;
   }
 
 };
+//added module.exports to make myObject available for use in other modules. This allows you to import and use myObject in other files.
 module.exports = {myObject};
 
+console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
