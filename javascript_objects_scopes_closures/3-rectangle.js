06674b4d84fd049f5707d81095b32ed2a764a572
Vegas90 <amosmwoni@gmail.com>
2023-10-03 16:43:46 +0300

    M

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        // If w or h is 0 or not a positive integer, create an empty object
+     return "Rectangle {}";
+      }
+        this.width = w;
+        this.height = h;
+    
+    }
+    print() {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+
+}
+  }
+  
+module.exports = Rectangle;  
