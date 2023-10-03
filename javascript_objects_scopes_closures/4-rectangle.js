0f26d787cb8d4a5d9ec7e7715106abae12390fd0
Vegas90 <amosmwoni@gmail.com>
2023-10-03 16:58:16 +0300

    M

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -15,12 +15,13 @@ class Rectangle {
         }
 
 }
-
+//Create an instance method called rotate() that exchanges the width and the height of the rectangle
 rotate(){
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
 }
+//Create an instance method called double() that multiples the width and the height of the rectangle by 2
 double(){
     this.width *= 2;
     this.height *= 2;
