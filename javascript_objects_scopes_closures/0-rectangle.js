aa643552d9d5a82eb9ea42ba24a9f96a6bca1e43
Vegas90 <amosmwoni@gmail.com>
2023-10-03 15:45:07 +0300

    M

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,9 +1,7 @@
 class Rectangle {
-    constructor() {
+    constructor()
+     {
       // Constructor can be defined here if needed
     }
 }
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.exports = Rectangle;
