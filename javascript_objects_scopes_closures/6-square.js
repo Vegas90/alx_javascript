efabda4b6d43c2df0ce1ef9b3be02cf74f14f42b
Vegas90 <amosmwoni@gmail.com>
2023-10-03 21:18:34 +0300

    M

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -35,11 +35,18 @@ class Square extends Rectangle
 constructor(size){
     super(size,size);
 }
+}
 
-charPrint(C) {
-    for (let i = 0; i < this.size; i++) {
-      console.log('C'.repeat(this.size));
+class Square extends require('./5-square') {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X'; // Default to 'X' if c is undefined
     }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Rectangle;  
