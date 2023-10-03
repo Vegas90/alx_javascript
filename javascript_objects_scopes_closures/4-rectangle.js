ebf8a95f844f494815be56654b56e5e9d2fdd41c
Vegas90 <amosmwoni@gmail.com>
2023-10-03 16:51:55 +0300

    M

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -15,6 +15,16 @@ class Rectangle {
         }
 
 }
+
+rotate(){
+    this.width=this.height;
+    this.height=this.width;
+}
+double(){
+    this.width=this.width*2;
+    this.height=this.height*2;
+
+}
   }
   
 module.exports = Rectangle;  
