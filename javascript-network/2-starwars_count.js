7c2b2e09a89160fad3ba09077835d605a9bade32
Vegas90 <amosmwoni@gmail.com>
2023-10-05 21:49:44 +0300

    M

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
 // Make a GET request to the Star Wars API
-request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     // If there is an error, log it and return
     console.error(`Error: ${error.message}`);
     return;
   }
   // Parse the response body as JSON
-  const movie = JSON.parse(body);
-  // Print the movie title
-  console.log(movie.title);
+  const movies = JSON.parse(body);
+  // Count the number of movies where Wedge Antilles is present
+  const wedgeAntillesMovies = movies.filter(movie => movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+  // Print the number of movies
+  console.log(wedgeAntillesMovies.length);
 });
