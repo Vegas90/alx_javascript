a2269d12cf35db7d049c2ffc9d3a2f5b1d1b57cc
Vegas90 <amosmwoni@gmail.com>
2023-10-05 21:57:24 +0300

    M

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -8,9 +8,10 @@ request.get(apiUrl, { encoding: 'utf-8' }, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const responseData = JSON.parse(body);
     const filmsWithWedge = responseData.results.filter((film) =>
+    //Filters the films where "Wedge Antilles" (character ID 18) is present by checking if the character's URL is in the characters array of each film.
       film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
     );
-
+//Prints the number of movies with Wedge Antilles.
     console.log(filmsWithWedge.length);
   }
 });
