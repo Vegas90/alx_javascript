66684a341c5598fa247ce6d96ea9f588b949cccb
Vegas90 <amosmwoni@gmail.com>
2023-10-05 21:42:17 +0300

    M

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,23 +4,13 @@ const req = require ('request');
 const fs = require ('fs');
 const url = process.argv[2];
 const file = process.argv[3];
- // Make a GET request to the URL
- request.get(url, { encoding: 'utf-8' }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      fs.writeFile(filePath, body, 'utf-8', (err) => {
-        if (!err) {
-          // No console output
-        }
-      });
-    }
-  });
-
-  
-  
-  
-  
-  
-  
+function saveWebpage(url, filePath) {
+    // Make a GET request to the URL
+    request.get(url, { encoding: 'utf-8' }, (error, response, body) => {
+      // Save the response body to a file
+      fs.writeFile(filePath, body);
+    });
+  }
 
 
 
