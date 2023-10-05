d50c4685709386ebb3091bff47cc8f364738357e
Vegas90 <amosmwoni@gmail.com>
2023-10-05 15:15:49 +0300

    M

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,4 +2,4 @@
 const req = require('request');
 const url = process.argv[2];
 req.get(url, {decoding: 'utf-8'})
-console.error('code: ${response.statusCode}')
+console.log('code: ${response.statusCode}')
