b0708381fb34f9507b66a79edc543a9d6779469b
Vegas90 <amosmwoni@gmail.com>
2023-10-05 16:39:01 +0300

    M

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -8,6 +8,6 @@ const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
 req.get(url, {decoding: 'utf-8'})
 .on('data', function (data) {
     const response = JSON.parse(data);
-    console.log(data.director);
+    console.log(response.director);
 }
 );
