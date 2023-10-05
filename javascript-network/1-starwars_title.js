e045d77c196d3879d001575f98ec77d79e16a808
Vegas90 <amosmwoni@gmail.com>
2023-10-05 21:08:49 +0300

    M

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,16 +1,13 @@
-const request = require('request');
-function getMovieTitle(id) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-  request.get(url, { decoding: 'utf-8' }, (error, response, body) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    const movie = JSON.parse(body);
-    if (movie.episode_id === id) {
-      console.log(movie.title);
-    } else {
-      console.log(`No Star Wars movie with episode ID ${id} found.`);
-    }
-  });
+#!/usr/bin/node
+const req = require ('request');
+const id = process.argv[2];
+
+const url = 'https://swapi-api.alx-tools.com/api/films/1';
+const parts = [];
+
+req.get(url, {decoding: 'utf-8'})
+.on('data', function (data) {
+    const response = JSON.parse(data);
+    console.log(response.title);
 }
+);
