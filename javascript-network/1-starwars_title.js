08f4e39bf3d0dd717f8665cba6953fc5b6f56ca3
Vegas90 <amosmwoni@gmail.com>
2023-10-05 16:27:32 +0300

    M

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,7 +2,7 @@
 const req = require ('request');
 const id = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/${id}'
+const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
 
 
 req.get(url, {decoding: 'utf-8'})
