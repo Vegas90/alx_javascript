878ed158215b1a1cba4c831c22ddf74e9c281450
Vegas90 <amosmwoni@gmail.com>
2023-10-05 21:31:27 +0300

    M

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,7 +3,8 @@ const req = require ('request');
 const url = '${process.argv[2]}18';
 
 req.get(url, {decoding: 'utf-8'})
+const response = JSON.parse(data);
 .on('data', function (data) {
-    
+    if (response.characters === "https://swapi-api.alx-tools.com/api/people/18/"}
 };
 
---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const req = require ('request');
+//It is a core module in Node.js that provides functionality for working with the file system, including reading from and writing to files, creating directories, and performing various file-related operations.
+const fs = require ('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+ // Make a GET request to the URL
+req.get(url, {encoding: 'utf-8'}, (error,response, body) =>
+{
+     // Save the response body to a file
+    fs.writeFile(filePath,body);
+});
+
+
+
+
