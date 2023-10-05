44bba08b0ad395bb9bd9fcc32b7dcc22d99140a8
Vegas90 <amosmwoni@gmail.com>
2023-10-05 15:13:03 +0300

    M

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,4 +3,3 @@ const req = require('request');
 const url = process.argv[2];
 req.get('url', {decoding: 'utf-8'})
 console.error('code: ${response.statusCode}')
-//Write a script that display the status code of a GET request.
