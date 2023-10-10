16368bf7409d894947c985dece6b6c648d4c7200
Vegas90 <amosmwoni@gmail.com>
2023-10-10 10:34:52 +0300

    M

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,8 +1,26 @@
-function createClassRoom(numberOfStudents){
-    function studentSeat (seat){
+function createClassRoom(numberOfStudents) {
+    // Define a function called studentSeat that takes a seat number and returns a function
+    function studentSeat(seat) {
+      // The returned function simply returns the seat number
+      return function () {
         return seat;
+      };
     }
-    let students = {
-        
+  
+    // Create an empty array to store the student seat functions
+    var students = [];
+  
+    // Use a loop to create and add seat functions to the students array
+    for (let i = 0; i < numberOfStudents; i++) {
+      // Call studentSeat with the seat number (i + 1) and push the returned function to students
+      students.push(studentSeat(i + 1));
     }
-}
+  
+    // Return the array containing the seat functions
+    return students;
+  }
+  
+  // Call createClassRoom with 10 students and store the result in classRoom
+  var classRoom = createClassRoom(10);
+  
+  
